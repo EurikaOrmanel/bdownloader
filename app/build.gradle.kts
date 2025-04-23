f5fdda6bef239e25ad3559d01c7595e64882ee3d
EurikaOrmanel <wworgba@gmail.com>
2025-04-23 13:32:17 +0000

    version 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,7 @@ android {
 }
 
 dependencies {
-    implementation(":bdownloader")
-
+    implementation(project(":bdownloader"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
